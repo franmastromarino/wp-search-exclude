0e73cf7c299fd9221e13284f9ca96f2b101baa6b
Roman Pronskiy <root@pronskiy.com>
2012-12-03 18:02:54 +0200

    - Updated plugin

---
+++ metabox.php
@@ -0,0 +1,7 @@
+<div class="misc-pub-section">
+    <label for="sep_exclude">
+        <input type="hidden" name="sep[hidden]" id="sep_hidden" value="1" />
+        <input type="checkbox" name="sep[exclude]" id="sep_exclude" value="1" <?php echo ($exclude) ? 'checked' : ''; ?> />
+        Exclude from Search Results
+    </label>
+</div>
