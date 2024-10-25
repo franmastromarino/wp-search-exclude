64850acb9bcc4f6d3772c6ff1dbf2417f7286e75
Martin Meza <martinmezacorreo@gmail.com>
2024-10-25 11:56:02 -0300

    FIX: undefined component in WP versions below 6.6

--- packages/gutenberg/metabox/index.js
+++ packages/gutenberg/metabox/index.js
@@ -6,7 +6,9 @@ import { useExcludedSettings } from '@qlse/store';
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { PluginDocumentSettingPanel } from '@wordpress/editor';
+
+//TODO: uncomment when WP 6.6 is the minimum version required
+// import { PluginDocumentSettingPanel } from '@wordpress/editor';
 import { CheckboxControl } from '@wordpress/components';
 import { useState, useEffect, useRef } from '@wordpress/element';
 import { select, subscribe } from '@wordpress/data';
@@ -16,6 +18,23 @@ import { select, subscribe } from '@wordpress/data';
 import { useExcludeMeta } from '../../helpers/hooks';
 
 export const Metabox = () => {
+	/**
+	 * PluginDocumentSettingPanel - Supporting multiple WordPress versions (fix undefined import in wp versions before 6.6)
+	 *
+	 *
+	 * https://make.wordpress.org/core/2024/06/18/editor-unified-extensibility-apis-in-6-6/
+	 *
+	 *
+	 * The wp.editPost and wp.editSite slots will continue to work without changes, but the old slot locations will be deprecated.
+	 * To avoid triggering console warnings, you can support both the new and old slots at the same time.
+	 *
+	 * Once you are ready to make WP 6.6 the minimum required version for your plugin, you should be able to drop the fallbacks and restore the initial code.
+	 */
+	const PluginDocumentSettingPanel =
+		wp.editor?.PluginDocumentSettingPanel ??
+		wp.editPost?.PluginDocumentSettingPanel ??
+		wp.editSite?.PluginDocumentSettingPanel;
+
 	const {
 		settingsExcluded: excluded,
 		hasResolvedSettingsExcluded,
@@ -67,7 +86,10 @@ export const Metabox = () => {
 	};
 
 	return (
-		<PluginDocumentSettingPanel title="Search Exclude">
+		<PluginDocumentSettingPanel
+			title="Search Exclude"
+			name="search-exclude"
+		>
 			<CheckboxControl
 				__nextHasNoMarginBottom
 				label={__('Exclude from Search Results', 'search-exclude')}
